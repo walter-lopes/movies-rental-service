eef60e20fc304849b8025eefef374434189b0024
walter-lopes <walter_vinicius@outlook.com>
2021-08-31 20:30:24 -0300

    created cart structure and tests based on requirements

---
+++ src/MoviesRentalService.Domain/Rental/Cart.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MoviesRentalService.Domain.Rental
+{
+    public class Cart
+    {
+        public Cart(Guid userId)
+        {
+            Id = Guid.NewGuid();
+            UserId = userId;
+            Items = new HashSet<CartItem>();
+        }
+
+        public Guid Id { get; set; }
+
+        public HashSet<CartItem> Items { get; set; }
+
+        public Guid UserId { get; set; }
+
+
+        public bool Add(CartItem item)
+        {
+            if (Items.Contains(item))
+            {
+                return false;
+            }
+
+            Items.Add(item);
+            return true;
+        }
+    }
+}
---
+++ tests/MoviesRentalService.UnitTests/Domain/CartTests.cs
@@ -0,0 +1,64 @@
+﻿using FluentAssertions;
+using MoviesRentalService.Domain.Catalog;
+using MoviesRentalService.Domain.Rental;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace MoviesRentalService.UnitTests.Domain
+{
+    public class CartTests
+    {
+        [Fact]
+        public void Should_Init_Cart()
+        {
+            var userId = Guid.NewGuid();
+
+            var cart = new Cart(userId);
+
+            cart.Id.Should().NotBeEmpty();
+            cart.UserId.Should().Be(userId);
+            cart.Items.Should().NotBeNull();
+            cart.Items.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void Should_Add_New_Item()
+        {
+            var userId = Guid.NewGuid();
+            var cart = new Cart(userId);
+            string name = "The lord of the rings - Return of the king";
+            string description = "Adventure movie";
+            int stock = 10;
+            decimal price = 10.5M;
+            var movie = new Movie(name, description, stock, price);
+            var cartItem = new CartItem(movie);
+
+            bool added = cart.Add(cartItem);
+
+            cart.Items.Should().HaveCount(1);
+            cart.Items.FirstOrDefault().MovieId.Should().Be(movie.Id);
+            cart.Items.FirstOrDefault().Price.Should().Be(movie.Price);
+            added.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Should_Not_Add_Same_Item()
+        {
+            var userId = Guid.NewGuid();
+            var cart = new Cart(userId);
+            string name = "The lord of the rings - Return of the king";
+            string description = "Adventure movie";
+            int stock = 10;
+            decimal price = 10.5M;
+
+            var movie = new Movie(name, description, stock, price);
+            var cartItem = new CartItem(movie);
+
+            cart.Add(cartItem);
+            bool addedTwice = cart.Add(cartItem);
+
+            addedTwice.Should().BeFalse();
+        }
+    }
+}
