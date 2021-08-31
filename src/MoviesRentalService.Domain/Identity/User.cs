24e8453ab43d102b5a6f60a0120ea03152204671
walter-lopes <walter_vinicius@outlook.com>
2021-08-31 20:02:05 -0300

    init the user modeling and test it.

---
+++ src/MoviesRentalService.Domain/Identity/User.cs
@@ -0,0 +1,21 @@
+﻿namespace MoviesRentalService.Domain.Identity
+{
+    public class User
+    {
+        public User(string name, string email, string password, string role)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+            Role = role;
+        }
+
+        public string Name { get; private set; }
+
+        public string Email { get; private set; }
+
+        public string Password { get; private set; }
+
+        public string Role { get; private set; }
+    }
+}
---
+++ tests/MoviesRentalService.UnitTests/Domain/UserTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using MoviesRentalService.Domain.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MoviesRentalService.UnitTests.Domain
+{
+    public class UserTests
+    {
+        [Fact]
+        public void Should_Init_User_Entity()
+        {
+            string name = "Michael Scott";
+            string email = "michael.scott@dundermifflin.com";
+            string password = "AJHDGAHFT53673ghGFGF";
+            string role = "customer";
+
+            var user = new User(name, email, password, role);
+
+            user.Email.Should().Be(email);
+            user.Name.Should().Be(name);
+            user.Password.Should().Be(password);
+            user.Role.Should().Be(role);
+        }
+    }
+}
