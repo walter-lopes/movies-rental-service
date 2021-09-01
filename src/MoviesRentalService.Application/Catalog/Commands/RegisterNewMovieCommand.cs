00c18e78b5d7cbdd03e0ff28d6bb78c3fbc69df5
walter-lopes <walter_vinicius@outlook.com>
2021-09-01 12:19:01 -0300

    created new command to register a movie

---
+++ src/MoviesRentalService.Application/Catalog/Commands/RegisterNewMovieCommand.cs
@@ -0,0 +1,13 @@
+﻿using Aloha.CQRS.Commands;
+
+namespace MoviesRentalService.Application.Catalog.Commands
+{
+    public class RegisterNewMovieCommand : ICommand
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
