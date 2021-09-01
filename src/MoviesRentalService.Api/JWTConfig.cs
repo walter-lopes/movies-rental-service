ae511757bddeae2ad754d84b767b62634c453c2d
walter-lopes <walter_vinicius@outlook.com>
2021-09-01 20:43:13 -0300

    added ioc in startup

---
+++ src/MoviesRentalService.Api/JWTConfig.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using MoviesRentalService.Infra.Identity;
+using System.Text;
+
+namespace MoviesRentalService.Api
+{
+    public static class JWTConfig
+    {
+        public static void AddJwt(this IServiceCollection services)
+        {
+            IConfiguration configuration;
+            using (var serviceProvider = services.BuildServiceProvider())
+            {
+                configuration = serviceProvider.GetService<IConfiguration>();
+            }
+            var options = configuration.GetOptions<JwtOptions>("jwt");
+            services.AddSingleton(options);
+            services.AddSingleton<IJwtHandler, JwtHandler>();
+            services.AddAuthentication()
+                .AddJwtBearer(cfg =>
+                {
+                    cfg.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(options.SecretKey)),
+                        ValidIssuer = options.Issuer,
+                        ValidAudience = options.ValidAudience,
+                        ValidateAudience = options.ValidateAudience,
+                        ValidateLifetime = options.ValidateLifetime
+                    };
+                });
+
+        }
+
+        internal static TModel GetOptions<TModel>(this IConfiguration configuration, string sectionName) where TModel : new()
+        {
+            var model = new TModel();
+            configuration.GetSection(sectionName).Bind(model);
+            return model;
+        }
+    }
+}
---
+++ src/MoviesRentalService.Api/Startup.cs
@@ -0,0 +1,83 @@
+using Aloha;
+using Aloha.CQRS.Commands;
+using Aloha.CQRS.Events;
+using Aloha.CQRS.Notifications;
+using Aloha.CQRS.Queries;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using MoviesRentalService.Application;
+using MoviesRentalService.Infra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesRentalService.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+            .AddAloha()
+            .AddCommandHandlers()
+            .AddInMemoryCommandDispatcher()
+            .AddEventHandlers()
+            .AddInMemoryEventDispatcher()
+            .AddQueryHandlers()
+            .AddInMemoryQueryDispatcher()
+            .AddInMemoryNotificationDispatcher()
+            .Build();
+
+            services.AddJwt();
+
+            services
+                .AddServices()
+                .AddRepositories()
+                .AddMongoDb(Configuration);
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MoviesRentalService.Api", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MoviesRentalService.Api v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
