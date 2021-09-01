ef5335f35525d09e403d65bdec982abb4bccbf24
walter-lopes <walter_vinicius@outlook.com>
2021-09-01 18:15:19 -0300

    created cart repository

---
+++ src/MoviesRentalService.Infra/Repositories/BaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Driver;
+
+namespace MoviesRentalService.Infra.Repositories
+{
+    public class BaseRepository<T>
+    {
+        protected IDbContext DbContext { get; set; }
+
+        protected IMongoCollection<T> Collection;
+
+        public BaseRepository(IDbContext context, string collection)
+        {
+            this.DbContext = context;
+            this.Collection = DbContext.Context.GetCollection<T>(collection);
+        }
+    }
+}
---
+++ src/MoviesRentalService.Infra/Repositories/CartRepository.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Driver;
+using MoviesRentalService.Domain.Rent;
+using MoviesRentalService.Domain.Rent.Repositories;
+using System;
+using System.Threading.Tasks;
+
+namespace MoviesRentalService.Infra.Repositories
+{
+    public class CartRepository : BaseRepository<Cart>, ICartRepository
+    {
+        public CartRepository(IDbContext context)
+            : base(context, "carts") { }
+
+        public async Task<Cart> GetByUserIdAsync(Guid userId)
+        {
+            var filter = Builders<Cart>.Filter.Where(x => x.UserId == userId);
+
+            return await Collection.Find(filter).FirstOrDefaultAsync();
+        }
+
+        public void Insert(Cart cart)
+        {
+            DbContext.AddCommand(async () => await Collection.InsertOneAsync(cart));
+        }
+
+        public void Update(Cart cart)
+        {
+            var filter = Builders<Cart>.Filter.Where(x => x.Id == cart.Id);
+
+            DbContext.AddCommand(async () => await Collection.FindOneAndReplaceAsync(c => c.Id == cart.Id, cart));
+        }
+    }
+}
