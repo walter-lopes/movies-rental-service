6d9f7bb1c0319da26e8001379ee33f286be18a87
walter-lopes <walter_vinicius@outlook.com>
2021-09-01 13:49:02 -0300

    created add movie to cart command.

---
+++ src/MoviesRentalService.Application/Rent/Command/AddMovieToCartCommand.cs
@@ -0,0 +1,12 @@
+﻿using Aloha.CQRS.Commands;
+using System;
+
+namespace MoviesRentalService.Application.Rent.Command
+{
+    public class AddMovieToCartCommand : ICommand
+    {
+        public Guid UserId { get; set; }
+
+        public Guid MovieId { get; set; }
+    }
+}
---
+++ src/MoviesRentalService.Application/Rent/CommandHandlers/AddMovieToCartCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using Aloha.CQRS.Commands;
+using MoviesRentalService.Application.Rent.Command;
+using MoviesRentalService.Domain.Catalog;
+using MoviesRentalService.Domain.Catalog.Repositories;
+using MoviesRentalService.Domain.Rent;
+using MoviesRentalService.Domain.Rent.Repositories;
+using System.Threading.Tasks;
+
+namespace MoviesRentalService.Application.Rent.CommandHandlers
+{
+    public class AddMovieToCartCommandHandler : ICommandHandler<AddMovieToCartCommand>
+    {
+        private readonly ICartRepository _cartRepository;
+        private readonly IMovieRepository _movieRepository;
+
+        public AddMovieToCartCommandHandler(ICartRepository cartRepository)
+        {
+            _cartRepository = cartRepository;
+        }
+
+        public async Task HandleAsync(AddMovieToCartCommand command)
+        {
+            var cartTask = _cartRepository.GetByUserIdAsync(command.UserId);
+            var movieTask = _movieRepository.GetByIdAsync(command.MovieId);
+
+            await Task.WhenAll(cartTask, movieTask);
+
+            Cart cart = cartTask.Result;
+            Movie movie = movieTask.Result;
+
+            var item = new RentalItem(movie);
+
+            cart.Add(item);
+
+            _cartRepository.Update(cart);
+        }
+    }
+}
